edfc33b5bc19ef2329370fe7c168f6d2a557397f
noobie-iv <noobie-iv@mail.ru>
2025-02-09 14:11:31 +1000

    Add dewarping SizeParams class

---
+++ src/dewarping/SizeParams.cpp
@@ -0,0 +1,66 @@
+/*
+    Scan Tailor - Interactive post-processing tool for scanned pages.
+    Copyright (C) 2015  Joseph Artsimovich <joseph.artsimovich@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "SizeParams.h"
+#include "XmlMarshaller.h"
+#include "XmlUnmarshaller.h"
+#include <QDomDocument>
+#include <QDomElement>
+#include <QString>
+
+namespace dewarping
+{
+
+namespace str
+{
+    static char const MODE[] = "mode";
+    static char const WIDTH[] = "width";
+    static char const HEIGHT[] = "height";
+    static char const DISTANCE[] = "distance";
+}
+
+SizeParams::SizeParams()
+    : m_mode(SizeMode::CALC_BY_AREA)
+    , m_width(1024)
+    , m_height(1024)
+    , m_distance(1024)
+{
+}
+
+SizeParams::SizeParams(QDomElement const& el)
+    : m_mode(el.attribute(str::MODE))
+    , m_width(el.attribute(str::WIDTH).toDouble())
+    , m_height(el.attribute(str::HEIGHT).toDouble())
+    , m_distance(el.attribute(str::DISTANCE).toDouble())
+{
+}
+
+QDomElement
+SizeParams::toXml(QDomDocument& doc, QString const& name) const
+{
+    XmlMarshaller marshaller(doc);
+
+    QDomElement el(doc.createElement(name));
+    el.setAttribute(str::MODE, m_mode.toString());
+    el.setAttribute(str::WIDTH, m_width);
+    el.setAttribute(str::HEIGHT, m_height);
+    el.setAttribute(str::DISTANCE, m_distance);
+    return el;
+}
+
+} // namespace dewarping
---
+++ src/dewarping/SizeParams.h
@@ -0,0 +1,88 @@
+/*
+    Scan Tailor - Interactive post-processing tool for scanned pages.
+    Copyright (C) 2015  Joseph Artsimovich <joseph.artsimovich@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DESKEW_SIZE_PARAMS_H_
+#define DESKEW_SIZE_PARAMS_H_
+
+#include "SizeMode.h"
+
+class QDomDocument;
+class QDomElement;
+class QString;
+
+namespace dewarping
+{
+
+class SizeParams
+{
+public:
+    SizeParams();
+
+    SizeParams(QDomElement const& el);
+
+    QDomElement toXml(QDomDocument& doc, QString const& name) const;
+
+    SizeMode mode() const
+    {
+        return m_mode;
+    }
+
+    void setMode(SizeMode mode)
+    {
+        m_mode = mode;
+    }
+
+    double width() const
+    {
+        return m_width;
+    }
+
+    void setWidth(double width)
+    {
+        m_width = width;
+    }
+
+    double height() const
+    {
+        return m_height;
+    }
+
+    void setHeight(double height)
+    {
+        m_height = height;
+    }
+
+    double distance() const
+    {
+        return m_distance;
+    }
+
+    void setDistance(double distance)
+    {
+        m_distance = distance;
+    }
+private:
+    SizeMode m_mode;
+    double m_width;
+    double m_height;
+    double m_distance;
+};
+
+} // namespace dewarping
+
+#endif
